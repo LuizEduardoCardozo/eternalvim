6ec2c71f1df7ee1560bb731b24c4eb081ce5fa0c
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2024-06-15 19:42:16 -0300

    adds rust and anothe languages to treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
--- treesitter configs
-      local tsconfig = require('nvim-treesitter.configs')
-      tsconfig.setup({
-        ensure_installed = { "lua" },
-        highlight = { enabled = true },
-        indent = { enabled = true } 
-      })
-    end
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    -- treesitter configs
+    local tsconfig = require('nvim-treesitter.configs')
+    tsconfig.setup({
+      ensure_installed = { "lua", "vim", "vimdoc", "c", "cmake", "toml", "json", "csv", "cpp", "go", "rust" },
+      highlight = { enabled = true },
+      indent = { enabled = true }
+    })
+  end
 }
